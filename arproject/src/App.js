3efcb85f59c04567fbabfc27c99b67e82050a4fc
Pavel Petrunkin <makesomecode@gmail.com>
2020-06-06 20:21:18 +0300

    add camera option

--- arproject/src/App.js
+++ arproject/src/App.js
@@ -2,14 +2,26 @@ import React from 'react';
 import './App.css';
 import Graph from "./Containers/Graph";
 import {eulerGraph, regularGraph, triangleGraph} from "./constants/graphs";
+import triangleSet from '../src/assets/images/triangle.fset';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a-scene>
-            <Graph vertexes={triangleGraph} />
-            <a-camera camera="active: true" look-controls={false} fov={30} zoom={0.5} />
+          <a-scene
+              vr-mode-ui="enabled: false;"
+              renderer="logarithmicDepthBuffer: true;"
+              embedded
+              arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;"
+          >
+            <a-nft type="nft"
+                  url="./src/assets/images/triangle.fset"
+                  smooth="true"
+                  smoothCount="10"
+                  smoothTolerance=".01"
+                  smoothThreshold="5">
+                <Graph vertexes={triangleGraph} />
+            </a-nft>
             <a-sky color="#ECECEC"/>
         </a-scene>
       </header>
