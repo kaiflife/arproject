ce713e855bfef2d0d93f9a10ca35b04e513e34f1
Pavel Petrunkin <makesomecode@gmail.com>
2020-06-07 21:33:48 +0300

    fix position of 3d object

--- arproject/src/App.js
+++ arproject/src/App.js
@@ -1,30 +1,10 @@
 import React from 'react';
 import './App.css';
-import Graph from "./Containers/Graph";
-import {eulerGraph, regularGraph, triangleGraph} from "./constants/graphs";
-import triangleSet from '../src/assets/images/triangle.fset';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          <a-scene
-              vr-mode-ui="enabled: false;"
-              renderer="logarithmicDepthBuffer: true;"
-              embedded
-              arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;"
-          >
-            <a-nft type="nft"
-                  url="./src/assets/images/triangle.fset"
-                  smooth="true"
-                  smoothCount="10"
-                  smoothTolerance=".01"
-                  smoothThreshold="5">
-                <Graph vertexes={triangleGraph} />
-            </a-nft>
-            <a-sky color="#ECECEC"/>
-        </a-scene>
-      </header>
+
     </div>
   );
 }
