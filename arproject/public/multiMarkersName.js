8c24e9b6e92034ecc7aa336e596e055f890f3b23
Pavel Petrunkin <makesomecode@gmail.com>
2020-06-14 04:08:38 +0300

    find multimarkers method

---
+++ arproject/public/multiMarkersName.js
@@ -0,0 +1,39 @@
+var markersURLArray=[];
+var markersNameArray=[];
+AFRAME.registerComponent('markers_start',{
+    init:function(){
+        console.log('Add markers to the scene');
+        const sceneEl = document.querySelector('a-scene');
+
+        //lists of the markers
+        for(let i=3; i<5; i++)
+        {
+            const url = `./pattern-${i}.patt`;
+            markersURLArray.push(url);
+            markersNameArray.push('Marker_'+i);
+        }
+        for(let k=0; k<2; k++)
+        {
+            const markerEl = document.createElement('a-marker');
+            markerEl.setAttribute('type','pattern');
+            markerEl.setAttribute('url',markersURLArray[k]);
+            markerEl.setAttribute('id',markersNameArray[k]);
+            markerEl.setAttribute('registerevents','');
+            sceneEl.appendChild(markerEl);
+        }
+    }
+});
+AFRAME.registerComponent('registerevents', {
+    init: function () {
+        const marker = this.el;
+        console.log('EGISTER EVENTS FOR MARKER')
+        marker.addEventListener("markerFound", ()=> {
+            const markerId = marker.id;
+            console.log('Marker Found: ', markerId);
+        });
+        marker.addEventListener("markerLost",() =>{
+            const markerId = marker.id;
+            console.log('Marker Lost: ', markerId);
+        });
+    },
+});
--- arproject/src/App.js
+++ arproject/src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
-import {triangleGraph} from "./constants/graphs";
+import {regularGraph, triangleGraph} from "./constants/graphs";
 import Graph from "./Containers/Graph";
+import ReactDOMServer from 'react-dom/server';
+
+const markerProps = [
+  {
+    id: 3,
+    url: './pattern-3.patt',
+    component: <Graph vertexes={triangleGraph}/>
+  },
+  {
+    id: 4,
+    url: './pattern-4.patt',
+    component: <Graph vertexes={regularGraph}/>
+  },
+];
 
 function App() {
-  return <Graph vertexes={triangleGraph} />;
+  useEffect(() => {
+    markerProps.forEach(item => {
+      const marker = document.querySelector(`#Marker_${item.id}`);
+      console.log(item.component);
+    })
+  }, []);
+  return <div />
 }
 
 export default App;
+
+
